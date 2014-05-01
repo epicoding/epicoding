7815b4806058cdb68e845031db9b34d11670f57a
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-05-01 00:17:10 -0700

    Add spec models and factories

---
+++ spec/factories/resources.rb
@@ -0,0 +1,11 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :resource, :class => 'Resources' do
+    name { Faker::Lorem.word }
+    summary { Faker::Lorem.sentences.join(" ") }
+    url 'http://www.epicodus.com'
+    user_id { Faker::Number.number(1) }
+    tool_id { Faker::Number.number(1) }
+  end
+end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,11 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
+###### This factoryGirl is not working! Email needs authentication. Check in rails c test.
+
 FactoryGirl.define do
   factory :user do
+    email { Faker::Internet.user_name }
+    password 'password'
+    password_confirmation { |u| u.password }
   end
 end
