244b8ad0a41c975caef677442a9abd82634e6b5a
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-05-01 00:16:38 -0700

    Add URL to resources table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501064123) do
+ActiveRecord::Schema.define(version: 20140501070358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20140501064123) do
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "url"
   end
 
   create_table "tools", force: true do |t|
