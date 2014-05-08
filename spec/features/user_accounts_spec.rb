d83b295a3dd9932c98721cdad72f18f354cf2d9f
Benjamin Paul Dunphy <dunphy.ben@gmail.com>
2014-05-08 16:19:26 -0700

    Get the sign_in capybara spec to pass

--- spec/features/user_accounts_spec.rb
+++ spec/features/user_accounts_spec.rb
@@ -9,15 +9,16 @@ feature 'user_accounts' do
 		fill_in :user_password, with: user.password
 		fill_in :user_password_confirmation, with: user.password
 		click_button "Sign up"
-
 		page.should have_content "Welcome! You have signed up successfully."
 	end
 
 	scenario 'an existing user signs in' do
 		user = FactoryGirl.create(:user)
-		visit "users/sign_in"
+		visit root_path
+		click_link 'Log in'
 		fill_in :user_email, with: user.email
 		fill_in :user_password, with: user.password
-		page.should have_content "Login successful."
+		click_button "Sign in"
+		page.should have_content user.email
 	end
 end
